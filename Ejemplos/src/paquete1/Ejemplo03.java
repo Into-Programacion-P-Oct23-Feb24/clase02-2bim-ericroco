a48e148c9a2764a2b3d2b2df9597c4ee4c8cd8de
ericroco <erickrodas559@gmail.com>
2023-12-13 16:22:54 -0500

    clase

--- Ejemplos/src/paquete1/Ejemplo03.java
+++ Ejemplos/src/paquete1/Ejemplo03.java
@@ -18,19 +18,24 @@ public class Ejemplo03 {
         // Declarar y generar arreglos en Java
 
         // arreglo de tipo entero
+        // te genera 5 espacios en blanco para que los llenes
         int[] arreglo1 = new int[5];
         
         // arreglo de tipo entero
+        //es una manera mas alargada de poner lo mismo de arriba
         int[] arreglo2;
         arreglo2 = new int[5];
         
         // arreglo de tipo entero
+        //llenamos el arreglo con datos ya definidos
         int[] arreglo3 = {5, 10, 15, 20, 25}; // sin new
         
         // arreglo de doubles - 3 elementos
+        //cuando usamos valores decimales, los escribimos con .
         double[] arreglo4 = {5.1, 10.2, 15.2}; // sin new
         
         // arreglo de cadenas - 4 elementos
+        //llenamos un arreglo d elementos de cadena
         String[] arreglo5 = {"Loja", "Quito", "Zamora", "Machala"};
     }
 
--- Ejemplos/src/paquete1/Ejemplo041.java
+++ Ejemplos/src/paquete1/Ejemplo041.java
@@ -9,26 +9,31 @@ package paquete1;
  *
  * @author reroes
  */
-public class Ejemplo04 {
+public class Ejemplo041 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // Declarar y generar arreglos en Java
-        
+        String acu = "";
+        int acu2 = 0;
         // arreglo de tipo entero
         //int[] arreglo1 = new int[5];
-         int[] arreglo1 = {1000, 1, 2, 2000, 300};
+        int[] arreglo1 = {1000, 1, 2, 2000, 300};
         // System.out.println(arreglo1.length);
         //arreglo1[2] = 1000;
         // el length me devuelve el numero de elementos q tiene el arreglo
         for (int i = 0; i < arreglo1.length; i++) {
-            System.out.printf("Subíndice o Índice %d, con valor %d\n",
-                    i, arreglo1[i]);
+            acu2 = acu2 + arreglo1[i];
+            acu = String.format("%sSubindice o indice %d, con valor %d\n",
+                    acu, i, arreglo1[i]);
         }
-        
-        
+        acu = String.format("%s\nLa Suma de estos valores es: %d",
+                 acu, acu2);
+        System.out.printf("%s\n",
+                acu.toUpperCase());
+
     }
 
 }
